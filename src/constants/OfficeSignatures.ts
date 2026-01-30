174191bc766ed59c01a2ed0e24e0ef9c79ec11d5
yash chopra <theyash920@gmail.com>
2026-01-30 15:17:27 +0530

    fixed wifi bssid issue

--- src/constants/OfficeSignatures.ts
+++ src/constants/OfficeSignatures.ts
@@ -1,4 +1,4 @@
 export const OfficeSignatures = {
-  BSSID: '14:D4:24:12:B5:6F', //office wifi bssid 14:D4:24:12:B5:6F 00:00:00:00:00:00
+  BSSID: 'A0:91:CA:9B:76:AA', //office wifi bssid 14:D4:24:12:B5:6F 00:00:00:00:00:00
   RSSI_THRESHOLD: -60,
 };
